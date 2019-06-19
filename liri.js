4b06a5184228a4d780959e28253bed9473063a68
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-18 23:11:48 -0700

    Changed filename of .env file to access KEY and SECRET, then configured node-spotify-api and axios

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+var songName = "";
+
 var spotify = new Spotify(keys.spotify);
+
+
+
