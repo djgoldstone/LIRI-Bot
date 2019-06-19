7822c8f73d45547d1d381a321617923e267ba63f
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-19 09:31:07 -0700

    Iterated through response JSON object and assigned a variable to the values of response.tracks.items[i] for a more concise way to access this informaiton. Then logged artist name information to console at index 0

--- liri.js
+++ liri.js
@@ -8,7 +8,11 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 spotify.search({ type: "track", query: "All The Small Things" }).then(function(response) {
-    console.log(JSON.stringify(response, null, 2));
+    for (var i = 0; i < response.tracks.items.length; i++){
+        var songData = response.tracks.items[i];
+        console.log("The artist is: " + songData.artists[0].name);
+    }
+    // console.log(JSON.stringify(response, null, 2));
 }).catch(function(err) {
     console.log(err);
 });
