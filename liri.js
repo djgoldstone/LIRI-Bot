3865fbc9b15370c67d28e669d9d6b9d09232828a
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-18 23:44:40 -0700

    Created search call to spotify api and applied JSON.stringify to the response

--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
-var axios = require("axios");
+// var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 
-var songName = "";
+// var songName = "";
 
 var spotify = new Spotify(keys.spotify);
 
-
-
+spotify.search({ type: "track", query: "All The Small Things" }).then(function(response) {
+    console.log(JSON.stringify(response, null, 2));
+}).catch(function(err) {
+    console.log(err);
+});
