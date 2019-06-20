5bd87d69f59d9639ce016566d9e46686b0383c9f
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-19 22:48:58 -0700

    Instatiated fs then assigned a command and args variable to process.argv. Declared a function to log spotify call and another function to execute do-what-it-says command. Created switch statement to determine which command is being issued and calls the corresponding function.

--- liri.js
+++ liri.js
@@ -1,13 +1,34 @@
-require("dotenv").config();
-// var axios = require("axios");
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-
-// var songName = "";
+var fs = require("fs");
+//variables assigned to require for each package
 
 var spotify = new Spotify(keys.spotify);
+//variable assigned spotify keys
 
-spotify.search({ type: "track", query: "El Cuarto De Tula" }).then(function(response) {
+var command = process.argv[2];
+var args = process.argv.slice(3);
+//variables assigned to the LIRI bot command and user input
+
+switch (command) {
+    case ("spotify-this-song"):
+        if (args.length > 0) {
+            spotifySongSearch(args);
+        } else {
+            spotifySongSearch("The Sign");
+        }
+    break;
+    case ("do-what-it-says"):
+        doWhatItSays();
+    break;
+    default:
+        console.log("Error: please try again...");
+}
+//switch statement that determines if a command has been issued and calls the corresponding function
+
+function spotifySongSearch(song) {
+spotify.search({ type: "track", query: song, limit: 1 }).then(function(response) {
     for (var i = 0; i < response.tracks.items.length; i++){
         var songData = response.tracks.items[i];
         console.log("--------------------------------------------");
@@ -21,3 +42,16 @@ spotify.search({ type: "track", query: "El Cuarto De Tula" }).then(function(resp
 }).catch(function(err) {
     console.log(err);
 });
+};
+//function that takes an argument of song and searches spotify's api for the queried song title and returns the artist name, song title, album name, and a preview of the song on spotify
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var txt = data.split(",");
+        if (txt[0] === "spotify-this-song") {
+            spotifySongSearch(txt[1]);
+        }
+        
+    })
+};
+//function that reads the random.txt file, splits the input data provided at the comma, and determines which command is being issued with a given input
